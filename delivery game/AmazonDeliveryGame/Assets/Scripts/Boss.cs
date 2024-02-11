52565a546f7dfee076a2602c4bbcccca00f038c1
hatim <hb1487@nyu.edu>
2024-02-11 14:54:48 -0500

    boss charges

--- delivery game/AmazonDeliveryGame/Assets/Scripts/Boss.cs
+++ delivery game/AmazonDeliveryGame/Assets/Scripts/Boss.cs
@@ -12,7 +12,14 @@ public class Boss : MonoBehaviour
     public Transform[] patrolPoints;
     public Transform currentPatrolPoint;
     private Transform prevPatrolPoint;
-    private State currentState;
+    [SerializeField]private State currentState;
+
+    public float patrolLength=5f; //in seconds
+    private float patrolTime=0f;
+
+    public float chargeLength=1f;
+    private float chargeTime=0f;
+    private float prevChargeTime=0f;
 
     void Start()
     {
@@ -38,6 +45,7 @@ public class Boss : MonoBehaviour
                 Patrol();
                 break;
             case State.Charging:
+                Charge();
                 break;
         }
         // if(Vector3.Distance(transform.position,player.position)>1f){
@@ -51,8 +59,45 @@ public class Boss : MonoBehaviour
         transform.LookAt(player);
     }
 
+    void Charge(){
+        if(chargeTime>chargeLength){
+            if(prevChargeTime<=chargeLength){
+                agent.SetDestination(player.position);
+                transform.LookAt(player);
+                //finding wall
+                Vector3 target=player.position;
+                Vector3 v=Vector3.Normalize(target-transform.position);
+                float maxDistance=100f;
+                RaycastHit hit;
+                int layerMask=1<<7;
+                if(Physics.Raycast(transform.position,v,out hit,maxDistance,layerMask)){
+                    target=transform.position+hit.distance*v;
+                    agent.SetDestination(target);
+                }
+            }else{
+                if(Vector3.Distance(agent.destination,transform.position)<1f){
+                    currentState=State.Patroling;
+                    chargeTime=0f;
+                    patrolTime=0f;
+                }
+            }
+        }else{
+            transform.LookAt(player);
+            agent.SetDestination(transform.position);
+        }
+        prevChargeTime=chargeTime;
+        chargeTime+=Time.deltaTime;
+    }
+
     void Patrol(){
-        if(Vector3.Distance(currentPatrolPoint.position,transform.position)<1f){
+        if(patrolTime>patrolLength){
+            currentState=State.Charging;
+            patrolTime=0f;
+            chargeTime=0f;
+            return;
+        }
+
+        if(patrolTime==0f || Vector3.Distance(currentPatrolPoint.position,transform.position)<1f){
             //Find new patrol point
             Transform newPatrolPoint=currentPatrolPoint;
             float minDistance=10000f;
@@ -65,9 +110,12 @@ public class Boss : MonoBehaviour
                     }
                 }
             }
+            Debug.Log("new patrol point");
             prevPatrolPoint=currentPatrolPoint;
             currentPatrolPoint=newPatrolPoint;
         }
         agent.SetDestination(currentPatrolPoint.position);
+
+        patrolTime+=Time.deltaTime;
     }
 }
