3cde07e930bc0eb8d55a6c847eee143ea92c4904
NicholeShinn <157531558+NicholeShinn@users.noreply.github.com>
2024-02-14 01:18:14 -0500

    Major Update
    dialogue scripts
    raycast change microbe filter
    sound

--- museum game/Assets/Scripts/Alien.cs
+++ museum game/Assets/Scripts/Alien.cs
@@ -1,12 +1,19 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Alien : MonoBehaviour
 {
+    public GameObject alienText;
+
+    //this function is called from Alien Interact Script. What happens here is what happens 
+    //here is what happens when you press E to interact with aliens
     public void Interact()
     {
-        // now the aliens just disappear
-        gameObject.SetActive(false);
+    
+        alienText.SetActive(true);
+
     }
+    
 }
