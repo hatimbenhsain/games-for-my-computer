ae074de9b228fbc3a0ccd074fcf52b31dcc171fd
IsabelleSmith21 <is2812@nyu.edu>
2024-03-09 23:55:37 -0500

    Dialogue Template Readded
    Deleted the old template and replaced it with the updated one with new dialog and boxes that move when speakers switch

--- YarnspinnerTemplate3D/Assets/Scripts/Stranger1TextBoxMove.cs
+++ YarnspinnerTemplate3D/Assets/Scripts/Stranger1TextBoxMove.cs
@@ -2,32 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using Yarn.Unity;//need this to be able to use yarn command
 
 public class Stranger1TextBoxMove : MonoBehaviour
 {
-    public Button clickButton;
     public GameObject objectToMove1;
     public GameObject objectToMove2;
-    public Button buttonToHide; // Declare the button you want to hide
     public float targetYCoordinate = 900f;
 
     void Start()
     {
-        // Attach the button click event to your custom method
-        clickButton.onClick.AddListener(OnClick);
+       
     }
 
     // Custom method to handle button click
-    void OnClick()
+
+    [YarnCommand]//hey yarn pay attention to this cause its a command
+    public void MoveBox1()//name of the command 
     {
-        // Hide the clicked button
-        buttonToHide.gameObject.SetActive(false);
-
-        // Move the assigned objects to the target Y coordinate
-        if (objectToMove1 != null)
-            objectToMove1.transform.position = new Vector3(objectToMove1.transform.position.x, targetYCoordinate, objectToMove1.transform.position.z);
-
-        if (objectToMove2 != null)
-            objectToMove2.transform.position = new Vector3(objectToMove2.transform.position.x, targetYCoordinate, objectToMove2.transform.position.z);
+        objectToMove1.transform.position = new Vector3(objectToMove1.transform.position.x, targetYCoordinate, objectToMove1.transform.position.z);//what we want to have happen when the command is called
+        objectToMove2.transform.position = new Vector3(objectToMove2.transform.position.x, targetYCoordinate, objectToMove2.transform.position.z);//what we want to have happen when the command is called
     }
 }
