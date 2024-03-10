2bd950fc3a9f090a0ff8af0db6a56c333d166659
NicholeShinn <157531558+NicholeShinn@users.noreply.github.com>
2024-03-10 11:13:27 -0400

    Moving Yarnspinner Template

---
+++ YarnspinnerTemplate3D/Assets/Scripts/Stranger2TextBoxMove.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Yarn.Unity;//need this to be able to use yarn command
+
+public class Stranger2TextBoxMove : MonoBehaviour
+{
+    public GameObject objectToMove1;
+    public GameObject objectToMove2;
+    public float targetYCoordinate = 80f;
+
+    void Start()
+    {
+       
+    }
+
+    // Custom method to handle button click
+
+    [YarnCommand]//hey yarn pay attention to this cause its a command
+    public void MoveBox2()//name of the command 
+    {
+        objectToMove1.transform.position = new Vector3(objectToMove1.transform.position.x, targetYCoordinate, objectToMove1.transform.position.z);//what we want to have happen when the command is called
+        objectToMove2.transform.position = new Vector3(objectToMove2.transform.position.x, targetYCoordinate, objectToMove2.transform.position.z);//what we want to have happen when the command is called
+    }
+}
