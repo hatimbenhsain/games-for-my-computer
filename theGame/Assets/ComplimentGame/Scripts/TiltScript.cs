91f50093bdce7e9771fae2473b5e2c1356749510
NicholeShinn <157531558+NicholeShinn@users.noreply.github.com>
2024-03-31 18:34:58 -0400

    PinBall Tilt Added
    changed the prefab a bit so it was less full of stuff. Replaced on collision w/ trigger & added tilt w/ rotations. Maybe someone can add rotation clamps later??

--- theGame/Assets/ComplimentGame/Scripts/TiltScript.cs
+++ theGame/Assets/ComplimentGame/Scripts/TiltScript.cs
@@ -1,19 +1,25 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class TiltScript : MonoBehaviour
 {
-    Rigidbody groundRigidbody;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float sensitivity = 0.5f;
 
+    public float mouseXValue;
+    public float mouseYValue;
+    
     // Update is called once per frame
     void Update()
-    {
-        
+    { 
+        //may need to mess around with sensitivity levels so the ball doesn't clip through the platform
+        mouseXValue = Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime;
+        mouseYValue = Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime;
+       
+       //using rotate is more consistent than physics, the physics kept breaking? 
+       transform.Rotate(-mouseYValue, 0, mouseXValue);
+       transform.rotation.Normalize();
+       
     }
 }
