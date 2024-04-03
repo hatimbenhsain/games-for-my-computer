83a4761e0a7db27c6a248f286d070a07340948c7
biageez <bc2814@nyu.edu>
2024-04-02 20:21:50 -0400

    Added clipping fixes for ball and added some limits to maze rotation that don't really work

--- theGame/Assets/ComplimentGame/Scripts/TiltScript.cs
+++ theGame/Assets/ComplimentGame/Scripts/TiltScript.cs
@@ -3,23 +3,42 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class TiltScript : MonoBehaviour
 {
     public float sensitivity = 0.5f;
 
     public float mouseXValue;
     public float mouseYValue;
-    
+
+    private Vector3 lastMousePosition;
+
+    private void Start()
+    {
+        //keep confined in game view
+        //Cursor.lockState = CursorLockMode.Confined;
+
+        //keep it at the center
+        //Cursor.lockState = CursorLockMode.Locked;
+    }
+
     // Update is called once per frame
     void Update()
     { 
         //may need to mess around with sensitivity levels so the ball doesn't clip through the platform
         mouseXValue = Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime;
         mouseYValue = Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime;
-       
-       //using rotate is more consistent than physics, the physics kept breaking? 
+
+        mouseXValue = Mathf.Min(mouseXValue, 0.1f);
+        mouseYValue = Mathf.Min(mouseYValue, 0.1f);
+        
+        // Sets target angle according to inputs + camera angle
        transform.Rotate(-mouseYValue, 0, mouseXValue);
+       // Smooths the target angle
        transform.rotation.Normalize();
-       
+
+       lastMousePosition = Input.mousePosition;
+
+
     }
 }
