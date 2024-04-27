b34a0b859c08dc36823f17cb11a3dea38135ca43
NicholeShinn <157531558+NicholeShinn@users.noreply.github.com>
2024-04-27 02:45:11 -0400

    dance scene update fever UI
    added 4 bar wrap around/fill--> beginnings of fever---> keys when pressed

--- theGame/Assets/DancingGame/Scripts/PartyLevelProgress.cs
+++ theGame/Assets/DancingGame/Scripts/PartyLevelProgress.cs
@@ -5,45 +5,63 @@ using UnityEngine.UI;
 
 public class PartyLevelProgress : MonoBehaviour
 {
-    public Slider progress;
+    public Image fillTop;
+    public Image fillRight;
+    public Image fillBottom;
+    public Image fillLeft;
     
     public float fillSpeed = 0.5f; //rate that the progress bar will fill from 0-->1
 
     public AudioSource soundBite01;
     public AudioSource soundBite02;
     public AudioSource soundBite03;
+
+    public GameObject Fever;
+
+    public GameObject PartyLevel;
     
 
     // Update is called once per frame
     void Update()
     {
+    fillingFeverBar();
+    }
+
+    void fillingFeverBar()
+    {
         // when any key is pressed the progress bar's value will fill by the fill speed, otherwise if no key is pressed it slides back down
         if (Input.anyKey)
         {
-            progress.value += fillSpeed * Time.deltaTime;
-        }
-        else
-        {
-            progress.value -= fillSpeed * Time.deltaTime;
-        }
-        
-        //could be fixed, kind of janky. If the progress value is between 0 & 0.5 "Seriously?" plays
-        if (progress.value >= 0 && progress.value <= 0.5f)
-        {
-            soundBite01.Play();
+            if (fillTop.fillAmount >= 0)
+            {
+                fillTop.fillAmount += fillSpeed * Time.deltaTime;
+            }
+            //fill bar 2 starts
+            
+            if (fillTop.fillAmount == 1)
+            {
+                fillRight.fillAmount += fillSpeed * Time.deltaTime;
+            } 
+            //fill bar 3 starts
+            
+            if (fillRight.fillAmount == 1)
+            {
+                fillBottom.fillAmount += fillSpeed * Time.deltaTime;
+            }
+            //final fill bar/left starts
+
+            if (fillBottom.fillAmount == 1)
+            {
+                fillLeft.fillAmount += fillSpeed * Time.deltaTime;
+            }
+
+            if (fillLeft.fillAmount == 1)
+            {
+                PartyLevel.GetComponent<Animator>().SetBool("FeverOn", true);
+                Fever.SetActive(true);
+            }
         }
 
-        //If the progress value between 0.6 & o.9 is achieved, play "clapping hands wooooo"
-        if (progress.value >= 0.6f && progress.value <= 0.9f)
-        {
-            soundBite03.Play();
-        }
-        //If progress bar reaches 1 play "wow shes really good!"
-        if (progress.value == 1)
-        {
-            soundBite02.Play();
-        }
     }
-
 
 }
