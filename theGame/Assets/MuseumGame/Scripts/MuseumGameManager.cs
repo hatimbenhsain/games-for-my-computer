052824fd78fd3783c4bc34d27d02738efdd5fe65
HanwenXuMame <128509287+HanwenXuMame@users.noreply.github.com>
2024-05-01 01:17:08 -0400

    museum added floating objects and fax prefab

--- theGame/Assets/MuseumGame/Scripts/MuseumGameManager.cs
+++ theGame/Assets/MuseumGame/Scripts/MuseumGameManager.cs
@@ -4,14 +4,29 @@ using UnityEngine;
 
 public class MuseumGameManager : MonoBehaviour
 {
-
+    public bool isFloating = true;
+    GameObject floatingObjects;
     void Start()
     {
+        floatingObjects = GameObject.Find("FloatingObjects");
         Cursor.visible = false;
     }
     
     void Update()
     {
-        
+        if (!isFloating)
+        {
+            DisableAllFloating(floatingObjects);
+        }
+    }
+
+    // make all the floating object not floating
+    private void DisableAllFloating(GameObject parent)
+    {
+        FloatingObjects[] floatingObjects = parent.GetComponentsInChildren<FloatingObjects>();
+        foreach (FloatingObjects floater in floatingObjects)
+        {
+            floater.enableFloating = false;
+        }
     }
 }
