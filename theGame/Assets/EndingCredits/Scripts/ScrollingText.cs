dc90d705a787a91108c8588f4e35f346c9f21716
zd <mamemoon@outlook.com>
2024-05-16 13:29:05 -0400

    small things

--- theGame/Assets/EndingCredits/Scripts/ScrollingText.cs
+++ theGame/Assets/EndingCredits/Scripts/ScrollingText.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using TMPro;
 
 public class ScrollingText : MonoBehaviour
@@ -34,6 +35,7 @@ public class ScrollingText : MonoBehaviour
                 }
                 else
                 {
+                    SceneManager.LoadScene("GameStart");
                     break;
                 }
             }
