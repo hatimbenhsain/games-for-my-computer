b7c4a85ceea116b0b725ba15151a90ee6e6b8f4e
zd <mamemoon@outlook.com>
2024-05-15 22:37:50 -0400

    very important

--- theGame/Assets/ComplimentGame/Scripts/ObjectResetter.cs
+++ theGame/Assets/ComplimentGame/Scripts/ObjectResetter.cs
@@ -4,14 +4,17 @@ public class ObjectResetter : MonoBehaviour
 {
     public GameObject assignableObject; // Drag the object to monitor here
     public Transform targetLocation;    // Drag the empty GameObject here to define the target location
+    private Transform startingPos;
 
     // This function is called when another object exits the trigger collider
-    private void OnTriggerExit(Collider other)
+    private void Start()
     {
-        // Check if the object leaving the collider is the assignable object
-        if (other.gameObject == assignableObject)
+        startingPos.position = assignableObject.transform.position;
+    }
+    private void Update()
+    {
+        if (assignableObject.transform.position.y < (startingPos.position.y - 20f) || (startingPos.position.y+100f) < assignableObject.transform.position.y)
         {
-            // Move the assignable object to the target location
             assignableObject.transform.position = targetLocation.position;
         }
     }
